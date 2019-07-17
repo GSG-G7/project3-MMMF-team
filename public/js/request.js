200bec6b7fb04c18382a2b07b527d1be0155384a
Mohammed-Q96 <mhmmade@gmail.com>
2019-07-17 10:11:13 +0300

    modify text relates #12

--- public/js/request.js
+++ public/js/request.js
@@ -3,7 +3,7 @@ function xhr(url, cb) {
    var xhr = new XMLHttpRequest();
    xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
-         cb(JSON.parse(xhr.responsetext));
+         cb(JSON.parse(xhr.responseText));
       }
    };
    xhr.open('GET', url);
